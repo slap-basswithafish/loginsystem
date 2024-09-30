6223e8b3ef852d67c979d4cae4a0fbb7dbbdf954
slap-basswithafish <141748516+slap-basswithafish@users.noreply.github.com>
2024-09-30 08:38:25 -0500

    awdas

---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="This is an example of a meta description. This will offten show up in search results.">
+        <meta name=viewport content="width=device-width, initial scale=1">
+        <title></title>
+        <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#">Portfolio</a></li>
+                <li><a href="#">About Me</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+            <div>
+                <form action="includes/login.inc.php" method="post">
+                    <input type="text" name="mailuid" placeholder="Username/E-mail...">
+                    <input type="password" name="pwd" placeholder="Password...">
+                    <button type="submit" name="login-submit">Login</button>
+                </form>
+                <a href="signup.php">Signup</a>
+                <form action="includes/logout.inc.php" method="post">  
+                    <button type="submit" name="logout-submit">Logout</button>
+                </form>
+            </div>
+        </nav>
+    </header>
